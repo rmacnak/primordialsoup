833d49087b91d7f21888a28471a28b0e689ef7f7
Ryan Macnak <rmacnak@gmail.com>
2017-08-15 21:14:11 -0700

    Update Windows synchronization code to CONDITION_VARIABLEs and SRWLOCKs.
    Requires Vista or later.

--- src/vm/os_thread_win.cc
+++ src/vm/os_thread_win.cc
@@ -48,8 +48,6 @@ static unsigned int __stdcall ThreadEntry(void* data_ptr) {
   uword parameter = data->parameter();
   delete data;
 
-  MonitorData::GetMonitorWaitDataForThread();
-
   // Create new OSThread object and set as TLS for new thread.
   OSThread* thread = OSThread::CreateOSThread();
   if (thread != NULL) {
@@ -59,9 +57,6 @@ static unsigned int __stdcall ThreadEntry(void* data_ptr) {
     // Call the supplied thread start function handing it its parameters.
     function(parameter);
   }
-
-  // Clean up the monitor wait data for this thread.
-  MonitorWaitData::ThreadExit();
 
   return 0;
 }
@@ -173,11 +168,7 @@ void OSThread::SetThreadLocal(ThreadLocalKey key, uword value) {
 
 
 Mutex::Mutex() {
-  // Allocate unnamed semaphore with initial count 1 and max count 1.
-  data_.semaphore_ = CreateSemaphore(NULL, 1, 1, NULL);
-  if (data_.semaphore_ == NULL) {
-    FATAL1("Mutex allocation failed %d", GetLastError());
-  }
+  InitializeSRWLock(&data_.lock_);
 #if defined(DEBUG)
   // When running with assertions enabled we do track the owner.
   owner_ = OSThread::kInvalidThreadId;
@@ -186,7 +177,6 @@ Mutex::Mutex() {
 
 
 Mutex::~Mutex() {
-  CloseHandle(data_.semaphore_);
 #if defined(DEBUG)
   // When running with assertions enabled we do track the owner.
   ASSERT(owner_ == OSThread::kInvalidThreadId);
@@ -195,10 +185,7 @@ Mutex::~Mutex() {
 
 
 void Mutex::Lock() {
-  DWORD result = WaitForSingleObject(data_.semaphore_, INFINITE);
-  if (result != WAIT_OBJECT_0) {
-    FATAL1("Mutex lock failed %d", GetLastError());
-  }
+  AcquireSRWLockExclusive(&data_.lock_);
 #if defined(DEBUG)
   // When running with assertions enabled we do track the owner.
   owner_ = OSThread::GetCurrentThreadId();
@@ -207,19 +194,13 @@ void Mutex::Lock() {
 
 
 bool Mutex::TryLock() {
-  // Attempt to pass the semaphore but return immediately.
-  DWORD result = WaitForSingleObject(data_.semaphore_, 0);
-  if (result == WAIT_OBJECT_0) {
+  if (TryAcquireSRWLockExclusive(&data_.lock_)) {
 #if defined(DEBUG)
     // When running with assertions enabled we do track the owner.
     owner_ = OSThread::GetCurrentThreadId();
 #endif  // defined(DEBUG)
     return true;
   }
-  if (result == WAIT_ABANDONED || result == WAIT_FAILED) {
-    FATAL1("Mutex try lock failed %d", GetLastError());
-  }
-  ASSERT(result == WAIT_TIMEOUT);
   return false;
 }
 
@@ -230,21 +211,13 @@ void Mutex::Unlock() {
   ASSERT(IsOwnedByCurrentThread());
   owner_ = OSThread::kInvalidThreadId;
 #endif  // defined(DEBUG)
-  BOOL result = ReleaseSemaphore(data_.semaphore_, 1, NULL);
-  if (result == 0) {
-    FATAL1("Mutex unlock failed %d", GetLastError());
-  }
+  ReleaseSRWLockExclusive(&data_.lock_);
 }
 
 
-ThreadLocalKey MonitorWaitData::monitor_wait_data_key_ = kUnsetThreadLocalKey;
-
-
 Monitor::Monitor() {
-  InitializeCriticalSection(&data_.cs_);
-  InitializeCriticalSection(&data_.waiters_cs_);
-  data_.waiters_head_ = NULL;
-  data_.waiters_tail_ = NULL;
+  InitializeSRWLock(&data_.lock_);
+  InitializeConditionVariable(&data_.cond_);
 
 #if defined(DEBUG)
   // When running with assertions enabled we track the owner.
@@ -258,29 +231,24 @@ Monitor::~Monitor() {
   // When running with assertions enabled we track the owner.
   ASSERT(owner_ == OSThread::kInvalidThreadId);
 #endif  // defined(DEBUG)
-
-  DeleteCriticalSection(&data_.cs_);
-  DeleteCriticalSection(&data_.waiters_cs_);
 }
 
 
 bool Monitor::TryEnter() {
-  // Attempt to pass the semaphore but return immediately.
-  BOOL result = TryEnterCriticalSection(&data_.cs_);
-  if (!result) {
-    return false;
-  }
+  if (TryAcquireSRWLockExclusive(&data_.lock_)) {
 #if defined(DEBUG)
-  // When running with assertions enabled we do track the owner.
-  ASSERT(owner_ == OSThread::kInvalidThreadId);
-  owner_ = OSThread::GetCurrentThreadId();
+    // When running with assertions enabled we do track the owner.
+    ASSERT(owner_ == OSThread::kInvalidThreadId);
+    owner_ = OSThread::GetCurrentThreadId();
 #endif  // defined(DEBUG)
-  return true;
+    return true;
+  }
+  return false;
 }
 
 
 void Monitor::Enter() {
-  EnterCriticalSection(&data_.cs_);
+  AcquireSRWLockExclusive(&data_.lock_);
 
 #if defined(DEBUG)
   // When running with assertions enabled we track the owner.
@@ -297,136 +265,7 @@ void Monitor::Exit() {
   owner_ = OSThread::kInvalidThreadId;
 #endif  // defined(DEBUG)
 
-  LeaveCriticalSection(&data_.cs_);
-}
-
-
-void MonitorWaitData::ThreadExit() {
-  if (MonitorWaitData::monitor_wait_data_key_ != kUnsetThreadLocalKey) {
-    uword raw_wait_data =
-        OSThread::GetThreadLocal(MonitorWaitData::monitor_wait_data_key_);
-    // Clear in case this is called a second time.
-    OSThread::SetThreadLocal(MonitorWaitData::monitor_wait_data_key_, 0);
-    if (raw_wait_data != 0) {
-      MonitorWaitData* wait_data =
-          reinterpret_cast<MonitorWaitData*>(raw_wait_data);
-      delete wait_data;
-    }
-  }
-}
-
-
-void MonitorData::AddWaiter(MonitorWaitData* wait_data) {
-  // Add the MonitorWaitData object to the list of objects waiting for
-  // this monitor.
-  EnterCriticalSection(&waiters_cs_);
-  if (waiters_tail_ == NULL) {
-    ASSERT(waiters_head_ == NULL);
-    waiters_head_ = waiters_tail_ = wait_data;
-  } else {
-    waiters_tail_->next_ = wait_data;
-    waiters_tail_ = wait_data;
-  }
-  LeaveCriticalSection(&waiters_cs_);
-}
-
-
-void MonitorData::RemoveWaiter(MonitorWaitData* wait_data) {
-  // Remove the MonitorWaitData object from the list of objects
-  // waiting for this monitor.
-  EnterCriticalSection(&waiters_cs_);
-  MonitorWaitData* previous = NULL;
-  MonitorWaitData* current = waiters_head_;
-  while (current != NULL) {
-    if (current == wait_data) {
-      if (waiters_head_ == waiters_tail_) {
-        waiters_head_ = waiters_tail_ = NULL;
-      } else if (current == waiters_head_) {
-        waiters_head_ = waiters_head_->next_;
-      } else if (current == waiters_tail_) {
-        ASSERT(previous != NULL);
-        waiters_tail_ = previous;
-        previous->next_ = NULL;
-      } else {
-        ASSERT(previous != NULL);
-        previous->next_ = current->next_;
-      }
-      // Clear next.
-      wait_data->next_ = NULL;
-      break;
-    }
-    previous = current;
-    current = current->next_;
-  }
-  LeaveCriticalSection(&waiters_cs_);
-}
-
-
-void MonitorData::SignalAndRemoveFirstWaiter() {
-  EnterCriticalSection(&waiters_cs_);
-  MonitorWaitData* first = waiters_head_;
-  if (first != NULL) {
-    // Remove from list.
-    if (waiters_head_ == waiters_tail_) {
-      waiters_tail_ = waiters_head_ = NULL;
-    } else {
-      waiters_head_ = waiters_head_->next_;
-    }
-    // Clear next.
-    first->next_ = NULL;
-    // Signal event.
-    BOOL result = SetEvent(first->event_);
-    if (result == 0) {
-      FATAL1("Monitor::Notify failed to signal event %d", GetLastError());
-    }
-  }
-  LeaveCriticalSection(&waiters_cs_);
-}
-
-
-void MonitorData::SignalAndRemoveAllWaiters() {
-  EnterCriticalSection(&waiters_cs_);
-  // Extract list to signal.
-  MonitorWaitData* current = waiters_head_;
-  // Clear list.
-  waiters_head_ = waiters_tail_ = NULL;
-  // Iterate and signal all events.
-  while (current != NULL) {
-    // Copy next.
-    MonitorWaitData* next = current->next_;
-    // Clear next.
-    current->next_ = NULL;
-    // Signal event.
-    BOOL result = SetEvent(current->event_);
-    if (result == 0) {
-      FATAL1("Failed to set event for NotifyAll %d", GetLastError());
-    }
-    current = next;
-  }
-  LeaveCriticalSection(&waiters_cs_);
-}
-
-
-MonitorWaitData* MonitorData::GetMonitorWaitDataForThread() {
-  // Ensure that the thread local key for monitor wait data objects is
-  // initialized.
-  ASSERT(MonitorWaitData::monitor_wait_data_key_ != kUnsetThreadLocalKey);
-
-  // Get the MonitorWaitData object containing the event for this
-  // thread from thread local storage. Create it if it does not exist.
-  uword raw_wait_data =
-      OSThread::GetThreadLocal(MonitorWaitData::monitor_wait_data_key_);
-  MonitorWaitData* wait_data = NULL;
-  if (raw_wait_data == 0) {
-    HANDLE event = CreateEvent(NULL, FALSE, FALSE, NULL);
-    wait_data = new MonitorWaitData(event);
-    OSThread::SetThreadLocal(MonitorWaitData::monitor_wait_data_key_,
-                             reinterpret_cast<uword>(wait_data));
-  } else {
-    wait_data = reinterpret_cast<MonitorWaitData*>(raw_wait_data);
-    wait_data->next_ = NULL;
-  }
-  return wait_data;
+  ReleaseSRWLockExclusive(&data_.lock_);
 }
 
 
@@ -439,41 +278,16 @@ Monitor::WaitResult Monitor::Wait(int64_t millis) {
 #endif  // defined(DEBUG)
 
   Monitor::WaitResult retval = kNotified;
-
-  // Get the wait data object containing the event to wait for.
-  MonitorWaitData* wait_data = MonitorData::GetMonitorWaitDataForThread();
-
-  // Start waiting by adding the MonitorWaitData to the list of
-  // waiters.
-  data_.AddWaiter(wait_data);
-
-  // Leave the monitor critical section while waiting.
-  LeaveCriticalSection(&data_.cs_);
-
-  // Perform the actual wait on the event.
-  DWORD result = WAIT_FAILED;
-  if (millis == 0) {
-    // Wait forever for a Notify or a NotifyAll event.
-    result = WaitForSingleObject(wait_data->event_, INFINITE);
-    if (result == WAIT_FAILED) {
-      FATAL1("Monitor::Wait failed %d", GetLastError());
-    }
+  if (millis == kNoTimeout) {
+    SleepConditionVariableSRW(&data_.cond_, &data_.lock_, INFINITE, 0);
   } else {
     // Wait for the given period of time for a Notify or a NotifyAll
     // event.
-    result = WaitForSingleObject(wait_data->event_, millis);
-    if (result == WAIT_FAILED) {
-      FATAL1("Monitor::Wait with timeout failed %d", GetLastError());
-    }
-    if (result == WAIT_TIMEOUT) {
-      // No longer waiting. Remove from the list of waiters.
-      data_.RemoveWaiter(wait_data);
+    if (!SleepConditionVariableSRW(&data_.cond_, &data_.lock_, millis, 0)) {
+      ASSERT(GetLastError() == ERROR_TIMEOUT);
       retval = kTimedOut;
     }
   }
-
-  // Reacquire the monitor critical section before continuing.
-  EnterCriticalSection(&data_.cs_);
 
 #if defined(DEBUG)
   // When running with assertions enabled we track the owner.
@@ -501,19 +315,14 @@ Monitor::WaitResult Monitor::WaitMicros(int64_t micros) {
 void Monitor::Notify() {
   // When running with assertions enabled we track the owner.
   ASSERT(IsOwnedByCurrentThread());
-  data_.SignalAndRemoveFirstWaiter();
+  WakeConditionVariable(&data_.cond_);
 }
 
 
 void Monitor::NotifyAll() {
   // When running with assertions enabled we track the owner.
   ASSERT(IsOwnedByCurrentThread());
-  // If one of the objects in the list of waiters wakes because of a
-  // timeout before we signal it, that object will get an extra
-  // signal. This will be treated as a spurious wake-up and is OK
-  // since all uses of monitors should recheck the condition after a
-  // Wait.
-  data_.SignalAndRemoveAllWaiters();
+  WakeAllConditionVariable(&data_.cond_);
 }
 
 
@@ -638,7 +447,6 @@ void NTAPI OnDartThreadExit(PVOID module, DWORD reason, PVOID reserved) {
   // and on W2K and W2K3. So don't assume it is sent.
   if (DLL_THREAD_DETACH == reason || DLL_PROCESS_DETACH == reason) {
     psoup::ThreadLocalData::RunDestructors();
-    psoup::MonitorWaitData::ThreadExit();
   }
 }
 
